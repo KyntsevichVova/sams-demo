2b86e3915b30272f37d08367e4b070f35777233b
kyntsevichvova <vkyntsevich@gmail.com>
2019-10-17 15:13:10 +0300

    Add simple React front

---
+++ src/main/java/com/sams/demo/HomeController.java
@@ -0,0 +1,10 @@
+package com.sams.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class HomeController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
