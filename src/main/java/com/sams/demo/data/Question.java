33db17c6d9d6c84975bdd02ee8c9b1df1365390e
Vladimir Kuntsevich <vkyntsevich@gmail.com>
2019-10-23 01:21:02 +0300

    Add constraints to Question

--- src/main/java/com/sams/demo/data/Question.java
+++ src/main/java/com/sams/demo/data/Question.java
@@ -1,6 +1,8 @@
 package com.sams.demo.data;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "QUESTION")
@@ -8,38 +10,49 @@ public class Question {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private final int id;
-    private final String title;
-    private final String link;
-    private final String level;
+    private int id;
 
-    private Question() {
-        id = 0;
-        title = null;
-        link = null;
-        level = null;
-    }
+    @NotNull
+    @Size(max = 100)
+    private String title;
 
-    public Question(int id, String title, String link, String level) {
-        this.id = id;
-        this.title = title;
-        this.link = link;
-        this.level = level;
-    }
+    @NotNull
+    @Size(max = 100)
+    private String link;
+
+    @NotNull
+    @Size(max = 10)
+    private String level;
 
     public int getId() {
         return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getTitle() {
         return title;
     }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     public String getLink() {
         return link;
     }
 
+    public void setLink(String link) {
+        this.link = link;
+    }
+
     public String getLevel() {
         return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
     }
 }
