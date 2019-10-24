0b0e93b4749d445800eeb6a80def7e87fe75cf52
Vladimir Kuntsevich <vkyntsevich@gmail.com>
2019-10-25 00:58:52 +0300

    Add lombok

--- src/main/java/com/sams/demo/data/Question.java
+++ src/main/java/com/sams/demo/data/Question.java
@@ -1,11 +1,14 @@
 package com.sams.demo.data;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "QUESTION")
+@Data
 public class Question {
 
     @Id
@@ -23,36 +26,4 @@ public class Question {
     @NotNull
     @Size(max = 10)
     private String level;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getLink() {
-        return link;
-    }
-
-    public void setLink(String link) {
-        this.link = link;
-    }
-
-    public String getLevel() {
-        return level;
-    }
-
-    public void setLevel(String level) {
-        this.level = level;
-    }
 }
