4ed2df7b4dee4b051f32733e751a8ceac76d3163
Vladimir Kuntsevich <vkyntsevich@gmail.com>
2019-10-25 00:59:28 +0300

    Add column mapping for Question

--- src/main/java/com/sams/demo/data/Question.java
+++ src/main/java/com/sams/demo/data/Question.java
@@ -13,17 +13,21 @@ public class Question {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ID", nullable = false)
     private int id;
 
     @NotNull
     @Size(max = 100)
+    @Column(name = "TITLE", nullable = false, length = 100)
     private String title;
 
     @NotNull
     @Size(max = 100)
+    @Column(name = "LINK", nullable = false, length = 100)
     private String link;
 
     @NotNull
     @Size(max = 10)
+    @Column(name = "LEVEL", nullable = false, length = 10)
     private String level;
 }
