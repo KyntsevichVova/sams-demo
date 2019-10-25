675ab2a300b9f0a28e4e4b583e3d6bb5eba3b9e6
kyntsevichvova <vkyntsevich@gmail.com>
2019-10-25 12:36:55 +0300

    Remove validation annotations from Question

--- src/main/java/com/sams/demo/data/Question.java
+++ src/main/java/com/sams/demo/data/Question.java
@@ -3,8 +3,6 @@ package com.sams.demo.data;
 import lombok.Data;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "QUESTION")
@@ -16,18 +14,12 @@ public class Question {
     @Column(name = "ID", nullable = false)
     private int id;
 
-    @NotNull
-    @Size(max = 255)
     @Column(name = "TITLE", nullable = false)
     private String title;
 
-    @NotNull
-    @Size(max = 255)
     @Column(name = "LINK", nullable = false)
     private String link;
 
-    @NotNull
-    @Size(max = 10)
     @Column(name = "LEVEL", nullable = false, length = 10)
     private String level;
 }
