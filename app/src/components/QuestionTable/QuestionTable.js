d98443f199512b4bdc695888f1fd34185fe17daf
kyntsevichvova <vkyntsevich@gmail.com>
2019-10-31 14:56:08 +0300

    Add FA icons for actions instead of link

--- app/src/components/QuestionTable/QuestionTable.js
+++ app/src/components/QuestionTable/QuestionTable.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function QuestionTable({posts, filter}) {
     return (
@@ -9,7 +10,10 @@ function QuestionTable({posts, filter}) {
                         Title
                     </th>
                     <th scope="col">
-                        Link
+                        Level
+                    </th>
+                    <th scope="col">
+                        Actions
                     </th>
                 </tr>
             </thead>
@@ -23,8 +27,17 @@ function QuestionTable({posts, filter}) {
                                 {value.title}
                             </td>
                             <td>
-                                <a target="_blank" rel="noopener noreferrer" href={value.link}>
-                                    {value.link}
+                                {value.level[0].toUpperCase() + value.level.substr(1)}
+                            </td>
+                            <td>
+                                <a className="text-primary mx-2" target="_blank" rel="noopener noreferrer" href={value.link}>
+                                    <FontAwesomeIcon icon={["fas", "external-link-alt"]} />
+                                </a>
+                                <a className="text-info mx-2" href="#">
+                                    <FontAwesomeIcon icon={["far", "edit"]} />
+                                </a>
+                                <a className="text-danger mx-2" href="#">
+                                    <FontAwesomeIcon icon={["far", "trash-alt"]} />
                                 </a>
                             </td>
                         </tr>
--- app/src/index.js
+++ app/src/index.js
@@ -3,5 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './components/App/App';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
+import { faEdit, faTrashAlt } from '@fortawesome/free-regular-svg-icons';
+
+library.add(faExternalLinkAlt, faEdit, faTrashAlt);
 
 ReactDOM.render(<App />, document.getElementById('root'));
