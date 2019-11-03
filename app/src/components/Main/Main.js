9d7de06c4eaa50894d086763b838fdcea9e5104d
Vladimir Kuntsevich <vkyntsevich@gmail.com>
2019-11-04 00:20:04 +0300

    Add stub elements for create and search

--- app/src/components/Main/Main.js
+++ app/src/components/Main/Main.js
@@ -4,6 +4,7 @@ import QuestionTable from '../QuestionTable/QuestionTable';
 import FilterAside from '../FilterAside/FilterAside';
 import PaginationNav from '../PaginationNav/PaginationNav';
 import Dropdown from '../Dropdown/Dropdown';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const filters = [
     {filter: "all", text: "All"},
@@ -67,24 +68,40 @@ function Main() {
             <div className="container-fluid mx-5">
                 <div className="row mb-3">
                     <div className="col-2"></div>
-                    <div className="col-10">
-                        <span className="pb-2 font-weight-bold text-info border-bottom border-info">
-                            Show
-                            <Dropdown title={`${limit}`}>
-                                {limits.map((value) => {
-                                    return (
-                                        <a 
+                    <div className="col-10 d-flex flex-row justify-content-between">
+                        <div>
+                            <span className="pb-2 font-weight-bold text-info border-bottom border-info">
+                                Show
+                                <Dropdown title={`${limit}`}>
+                                    {limits.map((value) => {
+                                        return (
+                                            <a 
                                             className="dropdown-item" 
                                             href="#"
                                             onClick={() => {setLimit(value)}}
-                                        >
-                                            {value}
-                                        </a>
-                                    );  
-                                })}
-                            </Dropdown>
-                            entries
-                        </span>
+                                            >
+                                                {value}
+                                            </a>
+                                        );  
+                                    })}
+                                </Dropdown>
+                                entries
+                            </span>
+                            <button className="btn btn-primary mx-5">
+                                <FontAwesomeIcon icon={["fas", "plus"]} />
+                            </button>
+                        </div>
+                        <div className="d-flex flex-row justify-content-end">
+                            <div className="input-group mb-0">
+                                <div className="input-group-prepend">
+                                    <span className="input-group-text">
+                                        <FontAwesomeIcon icon={["fas", "search"]} />
+                                    </span>
+                                </div>
+                                <input type="text" className="form-control h-100">
+                                </input>
+                            </div>
+                        </div>
                     </div>                    
                 </div>
                 <div className="row">
--- app/src/index.js
+++ app/src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './components/App/App';
 import './index.css';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
+import { faExternalLinkAlt, faPlus, faSearch } from '@fortawesome/free-solid-svg-icons';
 import { faEdit, faTrashAlt } from '@fortawesome/free-regular-svg-icons';
 
-library.add(faExternalLinkAlt, faEdit, faTrashAlt);
+library.add(faExternalLinkAlt, faEdit, faTrashAlt, faPlus, faSearch);
 
 ReactDOM.render(<App />, document.getElementById('root'));
