ff1fe5281863c10491cbf6d27eadc67320de73c8
kyntsevichvova <vkyntsevich@gmail.com>
2019-11-21 13:13:12 +0300

    Change paths to relative

--- app/src/Constraints.js
+++ app/src/Constraints.js
@@ -1,5 +1,5 @@
 export const BASE_SUBDIR = "demo";
-export const BASE_URL = `http://localhost:8085/${BASE_SUBDIR}`;
+export const BASE_URL = `/${BASE_SUBDIR}`;
 export const API_URL = `${BASE_URL}/api/v1`;
 export const QUESTIONS_ENDPOINT = `${API_URL}/questions`;
 
