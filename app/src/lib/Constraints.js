793d1d30f37cf1ad5778bc546c76b8702568a7a3
kyntsevichvova <vkyntsevich@gmail.com>
2019-11-25 16:54:50 +0300

    Add missing translations

--- app/src/lib/Constraints.js
+++ app/src/lib/Constraints.js
@@ -4,10 +4,10 @@ export const API_URL = `${BASE_URL}/api/v1`;
 export const QUESTIONS_ENDPOINT = `${API_URL}/questions`;
 
 export const FILTERS = [
-    {filter: "all", text: "All"},
-    {filter: "JUNIOR", text: "Junior"},
-    {filter: "MIDDLE", text: "Middle"},
-    {filter: "SENIOR", text: "Senior"},
+    {filter: "all", text: "all"},
+    {filter: "JUNIOR", text: "junior"},
+    {filter: "MIDDLE", text: "middle"},
+    {filter: "SENIOR", text: "senior"},
 ];
 
 export const PAGE_SIZES = [5, 10, 25, 50];
