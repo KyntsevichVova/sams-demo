456f3a9a262eea078f3972b75495bf0d9ffd6a2d
kyntsevichvova <vkyntsevich@gmail.com>
2019-12-10 12:17:45 +0300

    Change level input to button group

--- app/src/lib/Constraints.js
+++ app/src/lib/Constraints.js
@@ -10,6 +10,12 @@ export const FILTERS = [
     {filter: "SENIOR", text: "senior"},
 ];
 
+export const LEVELS = [
+    {filter: "JUNIOR", text: "junior"},
+    {filter: "MIDDLE", text: "middle"},
+    {filter: "SENIOR", text: "senior"}
+];
+
 export const PAGE_SIZES = [5, 10, 25, 50];
 
 export const LOCALE = {
