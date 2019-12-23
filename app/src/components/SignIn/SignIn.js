8ee6fd60e3133f0447978a696d4e1d33bb502de6
kyntsevichvova <vkyntsevich@gmail.com>
2019-12-23 18:11:33 +0300

    Fix missing container around forms

--- app/src/components/SignIn/SignIn.js
+++ app/src/components/SignIn/SignIn.js
@@ -13,48 +13,50 @@ function SignIn() {
     }, []);
 
     return (
-        <div className="d-flex flex-column">
-            <div className="form-group">
-                <label htmlFor="login">
-                    { t('label.login') }
-                </label>
+        <div className="container">
+            <div className="d-flex flex-column">
+                <div className="form-group">
+                    <label htmlFor="login">
+                        { t('label.login') }
+                    </label>
 
-                <input 
-                    type="text" 
-                    className="form-control" 
-                    id="login" 
-                    placeholder={ t('placeholder.login') }
-                    value={user.login}
-                    name="login"
-                    onChange={changeHandler}
-                />
-            </div>
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        id="login" 
+                        placeholder={ t('placeholder.login') }
+                        value={user.login}
+                        name="login"
+                        onChange={changeHandler}
+                    />
+                </div>
 
-            <div className="form-group">
-                <label htmlFor="password">
-                    { t('label.password') }
-                </label>
+                <div className="form-group">
+                    <label htmlFor="password">
+                        { t('label.password') }
+                    </label>
 
-                <input 
-                    type="password" 
-                    className="form-control" 
-                    id="password" 
-                    placeholder={ t('placeholder.password') } 
-                    value={user.password}
-                    name="password"
-                    onChange={changeHandler}
-                />
-            </div>
-            
-            <div className="d-flex flex-row justify-content-end">
-                <button 
-                    className="btn btn-primary" 
-                    onClick={() => {
-                        okCallback(user);
-                    }}
-                >
-                    { t('button.signin') }
-                </button>
+                    <input 
+                        type="password" 
+                        className="form-control" 
+                        id="password" 
+                        placeholder={ t('placeholder.password') } 
+                        value={user.password}
+                        name="password"
+                        onChange={changeHandler}
+                    />
+                </div>
+                
+                <div className="d-flex flex-row justify-content-end">
+                    <button 
+                        className="btn btn-primary" 
+                        onClick={() => {
+                            okCallback(user);
+                        }}
+                    >
+                        { t('button.signin') }
+                    </button>
+                </div>
             </div>
         </div>
     );
--- app/src/components/SignUp/SignUp.js
+++ app/src/components/SignUp/SignUp.js
@@ -13,64 +13,66 @@ function SignUp() {
     }, []);
 
     return (
-        <div className="d-flex flex-column">
-            <div className="form-group">
-                <label htmlFor="email">
-                    { t('label.email') }
-                </label>
+        <div className="container">
+            <div className="d-flex flex-column">
+                <div className="form-group">
+                    <label htmlFor="email">
+                        { t('label.email') }
+                    </label>
 
-                <input 
-                    type="email" 
-                    className="form-control" 
-                    id="email" 
-                    placeholder={ t('placeholder.email') }
-                    value={user.email}
-                    name="email"
-                    onChange={changeHandler}
-                />
-            </div>
+                    <input 
+                        type="email" 
+                        className="form-control" 
+                        id="email" 
+                        placeholder={ t('placeholder.email') }
+                        value={user.email}
+                        name="email"
+                        onChange={changeHandler}
+                    />
+                </div>
 
-            <div className="form-group">
-                <label htmlFor="login">
-                    { t('label.login') }
-                </label>
+                <div className="form-group">
+                    <label htmlFor="login">
+                        { t('label.login') }
+                    </label>
 
-                <input 
-                    type="text" 
-                    className="form-control" 
-                    id="login" 
-                    placeholder={ t('placeholder.login') }
-                    value={user.login}
-                    name="login"
-                    onChange={changeHandler}
-                />
-            </div>
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        id="login" 
+                        placeholder={ t('placeholder.login') }
+                        value={user.login}
+                        name="login"
+                        onChange={changeHandler}
+                    />
+                </div>
 
-            <div className="form-group">
-                <label htmlFor="password">
-                    { t('label.password') }
-                </label>
+                <div className="form-group">
+                    <label htmlFor="password">
+                        { t('label.password') }
+                    </label>
 
-                <input 
-                    type="password" 
-                    className="form-control" 
-                    id="password" 
-                    placeholder={ t('placeholder.password') } 
-                    value={user.password}
-                    name="password"
-                    onChange={changeHandler}
-                />
-            </div>
-            
-            <div className="d-flex flex-row justify-content-end">
-                <button 
-                    className="btn btn-primary" 
-                    onClick={() => {
-                        okCallback(user);
-                    }}
-                >
-                    { t('button.signup') }
-                </button>
+                    <input 
+                        type="password" 
+                        className="form-control" 
+                        id="password" 
+                        placeholder={ t('placeholder.password') } 
+                        value={user.password}
+                        name="password"
+                        onChange={changeHandler}
+                    />
+                </div>
+                
+                <div className="d-flex flex-row justify-content-end">
+                    <button 
+                        className="btn btn-primary" 
+                        onClick={() => {
+                            okCallback(user);
+                        }}
+                    >
+                        { t('button.signup') }
+                    </button>
+                </div>
             </div>
         </div>
     );
